1433001a43122510a549eece28fc5efdfce78058
GGmike <miketsun1212@gmail.com>
2025-06-21 13:27:27 +0800

    Minor debug, optimize

--- uniqueDT.py
+++ uniqueDT.py
@@ -2,13 +2,19 @@ from itertools import combinations
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 from config import config
-from main import X_train, y_train
 
-def uniqueDT(columns, max_combination, num_nodes):
+def uniqueDT(columns, max_combination, num_nodes, X_train, y_train):
+    print(f"Creating unique Decision Trees with {num_nodes} nodes and max {max_combination} combinations.")
+    print("Type of max_combination:", type(max_combination))
+    print("Type of num_nodes:", type(num_nodes))
+    print("Type of columns:", type(columns))
+    print(f"Columns used: {len(columns)}")
+    # print("Columns:", columns)
     base_learners = []
     used_combinations = []
-    all_combinations = list(combinations(columns, num_nodes))
-    selected_combinations = all_combinations[:min(max_combination, len(all_combinations))]
+    all_combinations = list(combinations(columns, int(num_nodes)))
+    print(f"Total combinations: {len(all_combinations)}")
+    selected_combinations = all_combinations[:min(int(max_combination), len(all_combinations))]
     for training_columns in selected_combinations:
         training_columns = list(training_columns)        
         X_sample = X_train[training_columns]
