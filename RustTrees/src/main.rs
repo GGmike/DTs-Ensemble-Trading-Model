d5d24f2b8017a7c20d73b30b2ac5b37da371d81d
GGmike <miketsun1212@gmail.com>
2025-06-23 23:52:44 +0800

    Rust uodates

--- RustTrees/src/main.rs
+++ RustTrees/src/main.rs
@@ -1,6 +1,7 @@
 use pyo3::prelude::*;
 use itertools::Itertools;
 
-fn main() {
-    println!("Hello, world!");
+#[pyfunction]
+fn column_combinations(py_columns: Vec<String>, r: usize) -> Vec<Vec<String>> {
+    py_columns.into_iter().combinations(r).collect()
 }
