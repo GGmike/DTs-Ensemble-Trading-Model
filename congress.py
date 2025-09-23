d25ab5b8890a06fc28b406cf37b77774747ed67d
GGmike <miketsun1212@gmail.com>
2025-09-23 20:56:23 +0800

    Add new Args, TODO: Try real-time

--- congress.py
+++ congress.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process config parameter.')
 
     parser.add_argument('--symbol', '-s', default=config['symbol'], help='Stock symbol (default: TSLA)')
+    parser.add_argument('--period', '-p', default=config['period'], help='Data period (default: 1D)')
     parser.add_argument('--threshold', '-t', default=config['threshold'], help='Congress Voting Threshold (default: 0.6)')
     parser.add_argument('--n_estimator', '-n', default=config['rf_n_est'], help='Random Forest Number of Estimators (default: 10000)')
     
@@ -14,6 +15,7 @@ if __name__ == "__main__":
     config['symbol'] = args.symbol
     config['threshold'] = float(args.threshold)
     config['rf_n_est'] = int(args.n_estimator)
+    config['period'] = args.period
 
     print(f"Running with config: {config}")
 
