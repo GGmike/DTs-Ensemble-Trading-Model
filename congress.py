cda21ff09e94bcb170e7d43a8d4ef3ea71e330c7
GGmike <miketsun1212@gmail.com>
2025-10-13 00:31:28 +0800

    Update Congress python, add new options, using new data source

--- congress.py
+++ congress.py
@@ -9,11 +9,13 @@ if __name__ == "__main__":
 
     parser.add_argument('--symbol', '-s', default=config['symbol'], help='Stock symbol (default: TSLA)')
     parser.add_argument('--period', '-p', default=config['period'], help='Data period (default: 1D)')
+    parser.add_argument('--limit', '-l', default=1000000, help='Data limit (default: 1000000)')
     parser.add_argument('--threshold', '-t', default=config['threshold'], help='Congress Voting Threshold (default: 0.6)')
     parser.add_argument('--n_estimator', '-n', default=config['rf_n_est'], help='Random Forest Number of Estimators (default: 10000)')
     
     args = parser.parse_args()
     config['symbol'] = args.symbol
+    config['limit'] = int(args.limit)
     config['threshold'] = float(args.threshold)
     config['rf_n_est'] = int(args.n_estimator)
     config['period'] = args.period
@@ -23,11 +25,11 @@ if __name__ == "__main__":
     
     main()
 
-    print("Testing new Binance Data Source")
-    df = get_historical_data('SOLUSDT','5m')
-    if df is not None:
-        print(df.head())
-        print(df.tail())
-        print(f"Dataframe shape: {df.shape}")
-    else:   
-        print("Failed to retrieve data.")
+    # print("Testing new Binance Data Source")
+    # get_historical_data('SOLUSDT','5m')
+    # if df is not None:
+    #     print(df.head())
+    #     print(df.tail())
+    #     print(f"Dataframe shape: {df.shape}")
+    # else:   
+    #     print("Failed to retrieve data.")
